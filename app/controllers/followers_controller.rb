b9178a1605254e81cd5bc07005961e7692272ad6
Rohail Rafique <rohailrafique@gmail.com>
2021-12-07 11:55:26 -0500

    follower-and-posts-routes

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,2 +1,25 @@
 class FollowersController < ApplicationController
+
+    def create
+        if @current_user.id = params[:follower_id] 
+            follower = Follower.create!(follower_params)
+            render json: follower, status: :created
+        else 
+            render json: {error: "User not logged in"}
+        end
+    end
+
+    def destroy
+        if @current_user.id = params[:follower_id] 
+            Follower.find(params[:id]).destroy
+            head :no_content
+        else
+            render json: {error: "User not logged in"}
+        end
+
+    private
+
+    def follower_params
+        params.permit(:follower_id, :followed_id)
+    end
 end
